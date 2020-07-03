7361b9b4e84041532aaaea1f750b9f818f9332c3
Josh Curtis <jcurtis825@gmail.com>
2020-07-03 15:28:01 -0700

    MediaControlView#setOnFullScreenlistener(null) doesn't hide the fullscreen button

--- app/src/main/java/com/joshcurtis/androidbugs/MainActivity.kt
+++ app/src/main/java/com/joshcurtis/androidbugs/MainActivity.kt
@@ -2,10 +2,19 @@ package com.joshcurtis.androidbugs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.widget.ImageButton
+import androidx.media2.widget.MediaControlView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view: MediaControlView = findViewById(R.id.media_controller)
+        view.setOnFullScreenListener(null)
+        /* workaround to hide the fullscreen button
+        val button: ImageButton = findViewById(R.id.minimal_fullscreen)
+        button.visibility = GONE
+         */
     }
 }
